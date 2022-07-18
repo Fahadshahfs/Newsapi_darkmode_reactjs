e49c1b4d6aabf6ad62e1a57d1d988ba1f1b39acf
Fahad Shah FS <102901968+Fahadshahfs@users.noreply.github.com>
2022-07-19 02:09:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-
-import "./App.css";
 import MainNews from "./components/MainNews";
 import Navbar from "./components/Navbar";
 
